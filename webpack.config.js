b08a77658d5e7b38b86e946a2f6840fdcc9b5631
Eldar <eldar.hidirow@gmail.com>
2022-02-22 09:25:06 +0300

    Добавил webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/js/main.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 3333
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
